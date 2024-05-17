64b369c577522bd0f31fa98b2b7676434f78a476
Jamiexu <doxm@foxmail.com>
2024-05-17 10:10:21 +0800

    nothing

--- Users/Src/main.c
+++ Users/Src/main.c
@@ -2,11 +2,12 @@
 
 int main(void)
 {
+    SystemInit();
     systick_config();
     gpio_config();
     // spi_config();
     usart_config();
-    ST7735S_Init();
+    //    ST7735S_Init();
     uint8_t i;
 
     printf("Running...\n");
@@ -14,9 +15,9 @@ int main(void)
     {
         printf("Testing...\n");
         /* code */
-        LCD_LIGHT_LOW;
-        delay_1us(100); 
-        LCD_LIGHT_HIGH;
+        //        LCD_LIGHT_LOW;
+        //        delay_1us(100);
+        //        LCD_LIGHT_HIGH;
         delay_1ms(1000);
     }
 }
