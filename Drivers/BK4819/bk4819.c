1100f732b9f90066f73b6289ba29811a91e1bf88
Jamiexu <doxm@foxmail.com>
2024-05-24 17:34:17 +0800

    BK4819

--- Drivers/BK4819/bk4819.c
+++ Drivers/BK4819/bk4819.c
@@ -4,16 +4,18 @@
 static void spi_write_byte(uint8_t data)
 {
     BK4819_SCK_LOW;
+    BK4819_SDA_DIR_OUT;
     for (uint8_t i = 0; i < 8; i++)
     {
-        if (data & 0x80)
+        if ((data & 0x80) != 0x00)
             BK4819_SDA_HIGH;
         else
             BK4819_SDA_LOW;
-        bk4819_delay(1);
+        // bk4819_delay(1);
         BK4819_SCK_HIGH;
         bk4819_delay(1);
         BK4819_SCK_LOW;
+        bk4819_delay(1);
         data <<= 1;
     }
 }
@@ -27,16 +29,15 @@ static void spi_write_half_word(uint16_t data)
 static uint16_t spi_read_half_word(void)
 {
     uint16_t data = 0;
+    BK4819_SDA_DIR_IN;
     BK4819_SCK_LOW;
-    // BK4819_SCK_HIGH;
     for (uint8_t i = 0; i < 16; i++)
     {
         data <<= 1;
+        data |= BK4819_SDA_READ;
         BK4819_SCK_HIGH;
         bk4819_delay(1);
         BK4819_SCK_LOW;
-        // bk4819_delay(1);
-        data |= BK4819_SDA_READ;
         bk4819_delay(1);
     }
     return data;
@@ -46,36 +47,33 @@ uint16_t bk4819_read_reg(bk4819_reg_t reg)
 {
     uint16_t data;
     BK4819_SCN_LOW;
-    // bk4819_delay(1);
+    bk4819_delay(1);
 
     spi_write_byte(reg | BK4819_REG_READ);
-    bk4819_delay(1);
     data = spi_read_half_word();
 
-    // bk4819_delay(1);
+    bk4819_delay(1);
     BK4819_SCN_HIGH;
-
+    delay_1ms(1000);
     return data;
 }
 
 void bk4819_write_reg(bk4819_reg_t reg, uint16_t data)
 {
     BK4819_SCN_LOW;
-    // bk4819_delay(1);
+    bk4819_delay(1);
 
     spi_write_byte(reg | BK4819_REG_WRITE);
     spi_write_half_word(data);
 
-    // bk4819_delay(1);
+    bk4819_delay(1);
     BK4819_SCN_HIGH;
 }
 
 void bk4819_init(void)
 {
-    // BK4819_SCK_HIGH;
-    // BK4819_SDA_HIGH;
-    // BK4819_SCN_HIGH;
-    bk4819_write_reg(BK4819_REG_00, 0x8888);
+    uint8_t data;
+    bk4819_write_reg(BK4819_REG_00, 0x8000);
     bk4819_delay(1000);
     bk4819_write_reg(BK4819_REG_00, 0x00);
 }
