1c0fffe70e17805729193b895e3fbdf2a4d485b7
Jamiexu <doxm@foxmail.com>
2024-06-03 18:39:56 +0800

    Key

---
+++ Drivers/Key/key.c
@@ -0,0 +1,11 @@
+#include "key.h"
+
+// Written by Jamiexu
+
+void Key_Scan(void){
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        
+    }
+    
+}
---
+++ Drivers/Key/key.h
@@ -0,0 +1,9 @@
+#ifndef __KEY_JAMIEXU_H__
+#define __KEY_JAMIEXU_H__
+#include "main.h"
+
+// Written by Jamiexu
+
+void Key_Scan(void);
+
+#endif
