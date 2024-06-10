a9e16ebbfe6f1d672deab09886f5b15410e6131b
Jamie793 <doxm@foxmail.com>
2024-06-10 17:19:33 +0800

    substitute MinGW Makefiles for Ninja

--- CMake/arm_gcc_toolchain.cmake
+++ CMake/arm_gcc_toolchain.cmake
@@ -35,7 +35,7 @@ set(CMAKE_C_FLAGS  "-std=c99 -w -mthumb --specs=nosys.specs -mcpu=cortex-m4 -mfl
 
 # set(CMAKE_ASM_FLAGS "-mcpu=cortex-m4" CACHE INTERNAL "ASM Compiler options")
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-mcpu=cortex-m4 -mfloat-abi=soft -Wl,--gc-sections -flto -specs=nano.specs -T ${CMAKE_CURRENT_SOURCE_DIR}/gd32f3x0.ld -Xlinker -Map=output.map ")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-mcpu=cortex-m4 --specs=nosys.specs -mfloat-abi=soft -Wl,--gc-sections -flto -T ${CMAKE_CURRENT_SOURCE_DIR}/gd32f3x0.ld -Xlinker -Map=output.map ")
 
 
 message(STATUS "gcc toolchain is used")
