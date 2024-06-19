670cc42fa8c93ea76a45db612079832f4ae44605
Jamiexu <doxm@foxmail.com>
2024-06-19 18:48:17 +0800

    test file and testing lvgl

--- Users/Src/main.c
+++ Users/Src/main.c
@@ -1,10 +1,10 @@
 /**
  * @file main.c
  * @author Jamiexu (doxm@foxmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-05-11
- * 
+ *
  * @copyright MIT License
 
 Copyright (c) 2024 (Jamiexu or Jamie793)
@@ -26,7 +26,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
- * 
+ *
  */
 #include "main.h"
 #include "string.h"
@@ -36,95 +36,18 @@ SOFTWARE.
 
 extern void vtasks_init(void);
 
-
-uint8_t usart_tx[10];
-uint8_t flash_data[W25Q16JV_SECTOR_SIZE] = {0};
-uint8_t flash_data_text[16] = {0};
-uint32_t current_addr = 0;
-
-
-void flash_dump(void)
+void vApplicationStackOverflowHook(TaskHandle_t xTask,
+                                   char *pcTaskName)
 {
-    // printf("EEPROM WEL: %d\r\n", w25q16jv_read_reg1(W25Q16JV_REG1_WEL));
-    // printf("0x%08x     ", current_addr);
-    // for (uint8_t i = 0; i < 32; i++)
-    // {
-    //     // printf("**************BLOCK %d*****************\r\n", i);
-    //     for (uint8_t j = 0; j < 16; j++)
-    //     {
-    //         /* code */
-    //         // printf("**************SECTOR %d*****************\r\n", j);
-    //         w25q16jv_read_sector((i + 1) * j * W25Q16JV_SECTOR_SIZE - j * W25Q16JV_SECTOR_SIZE, flash_data);
-    //         for (uint32_t k = 0; k < W25Q16JV_SECTOR_SIZE; k++)
-    //         {
-    //             // printf("**************PAGE %d*****************\r\n", k);
-    //             sprintf(flash_data_text[k % 16], "%s", flash_data[i]);
-    //             printf("%02x ", flash_data[k]);
-    //             current_addr++;
-
-    //             if (current_addr % 16 == 0)
-    //                 printf("    %s\n0x%08x     ", flash_data_text, current_addr);
-    //             // printf("**************END PAGE %d**	***************\r\n", k);
-    //             // delay_1ms(50);
-    //         }
-    //         // printf("**************EEND SECTOR %d*****************\r\n", j);
-    //         // delay_1ms(50);
-    //     }
-    //     // printf("**************END BLOCK %d**************\r\n", i);
-    // }
+    /* Check pcTaskName for the name of the offending task,
+     * or pxCurrentTCB if pcTaskName has itself been corrupted. */
+    (void)xTask;
+    (void)pcTaskName;
 }
-
-void bk4819_test(void)
-{
-    // bk4819_write_reg(BK4819_REG_10, 0x8085);
-    // printf("BK4819 REG_10: 0x%x\r\n", bk4819_read_reg(BK4819_REG_10));
-    // printf("BK4819 REG_11: 0x%x\r\n", bk4819_read_reg(BK4819_REG_11));
-    // printf("BK4819 REG_12: 0x%x\r\n", bk4819_read_reg(BK4819_REG_12));
-    // printf("BK4819 REG_13: 0x%x\r\n", bk4819_read_reg(BK4819_REG_13));
-    // printf("BK4819 REG_14: 0x%x\r\n", bk4819_read_reg(BK4819_REG_14));
-    // printf("BK4819 REG_18: 0x%x\r\n", bk4819_read_reg(BK4819_REG_18));
-    // printf("BK4819 REG_19: 0x%x\r\n", bk4819_read_reg(BK4819_REG_19));
-
-    printf("Running...\r\n");
-    bk4819_set_freq(46255000);
-    printf("Current frequency: %d", (bk4819_read_reg(0x38) << 16 + bk4819_read_reg(0x39))*10);
-    printf("RSSI: %d\r\n", bk4819_read_reg(0x67) & 0x0F);
-    printf("Ex-noise: %d\r\n", bk4819_read_reg(0x65) & 0x0b111111);
-    printf("Glitch: %d\r\n", bk4819_read_reg(0x63) & 0x0b01111111);
-    printf("Squelch: %d\r\n", bk4819_read_reg(0x0C) & 0x01);
-}
-
-void bk1080_test(void)
-{
-
-    // bk1080_write_reg(BK1080_REG2, 0x8508);
-    // printf("Bk1080 REG0: 0x%x\r\n", bk1080_read_reg(BK1080_REG0));    // default 0x0006
-    printf("Bk1080 REG1: 0x%x\r\n", bk1080_read_reg(BK1080_REG1)); // default 0x1080
-    // printf("Bk1080 REG2: 0x%x\r\n", bk1080_read_reg(BK1080_REG2));    // default 0x0000
-    // printf("Bk1080 REG3: 0x%x\r\n", bk1080_read_reg(BK1080_REG3));    // default 0x0000
-    // printf("Bk1080 REG4: 0x%x\r\n", bk1080_read_reg(BK1080_REG4));    // default 0x0000
-    // printf("Bk1080 REG5: 0x%x\r\n", bk1080_read_reg(BK1080_REG5));    // default 0x0000
-    // printf("Bk1080 REG6: 0x%x\r\n", bk1080_read_reg(BK1080_REG6));    // default 0x0000
-    // printf("Bk1080 REG7: 0x%x\r\n", bk1080_read_reg(BK1080_REG7));    // default 0x0000
-    // printf("Bk1080 REG8: 0x%x\r\n", bk1080_read_reg(BK1080_REG8));    // default 0x0000
-    // printf("Bk1080 REG9: 0x%x\r\n", bk1080_read_reg(BK1080_REG9));    // default 0x0000
-    // printf("Bk1080 REG10: 0x%x\r\n", bk1080_read_reg(BK1080_REG10));  // default 0x0000
-    // printf("Bk1080 REG11: 0x%x\r\n", bk1080_read_reg(BK1080_REG11)); // default 0x0000
-
-    delay_1ms(1000);
-}
-
-void vApplicationStackOverflowHook( TaskHandle_t xTask,
-                                        char * pcTaskName )
-    {
-        /* Check pcTaskName for the name of the offending task,
-         * or pxCurrentTCB if pcTaskName has itself been corrupted. */
-        ( void ) xTask;
-        ( void ) pcTaskName;
-    }
 
 int main(void)
 {
+
     systick_config();
     gpio_config();
     nvic_config();
@@ -132,18 +55,31 @@ int main(void)
     spi_config();
     tim_config();
     usart_config();
+    st7735s_init();
 
-    // st7735s_init();
     // bk4819_init();
+    lv_init();
+    lv_port_disp_init();
+
+    /*Change the active screen's background color*/
+    // st7735s_set_color(0xFF, 0x00, 0x00);
+    // lv_obj_t *btn = lv_btn_create(lv_scr_act(), NULL); /*Add a button to the current screen*/
+    // lv_obj_set_pos(btn, 10, 10);                       /*Set its position*/
+    // lv_obj_set_size(btn, 100, 50);                     /*Set its size*/
+
     
-    vtasks_init();
-    vTaskStartScheduler();
-    
+    lv_obj_t *label = lv_label_create(lv_scr_act(), NULL);   /*Add a label to the button*/
+    lv_label_set_text(label, "Testing");           /*Set the labels text*/
+    lv_obj_set_pos(label, 30, 30); 
+    lv_obj_set_style_local_text_color(label, LV_LABEL_PART_MAIN, LV_STATE_DEFAULT, lv_color_hex(0xFF0000));
+
+    // vtasks_init();
+    // vTaskStartScheduler();
 
     // printf("Running...\r\n");
     while (1)
     {
-
+        lv_task_handler();
         // if (++time1_current_ms >= 1000){
         //     printf("1s...\r\n");
         //     time1_current_ms = 0;
@@ -168,11 +104,11 @@ int main(void)
         //     delay_1ms(500);
         // }
 
-        //st7735s_test();
-        // delay_1ms(1000);
-        // LCD_LIGHT_LOW;
-        // delay_1ms(1000);
-        // LCD_LIGHT_HIGH;
-        // delay_1ms(1000);
+        //  st7735s_test();
+        //  delay_1ms(1000);
+        //  LCD_LIGHT_LOW;
+        //  delay_1ms(1000);
+        //  LCD_LIGHT_HIGH;
+        //  delay_1ms(1000);
     }
 }
---
+++ Users/Src/test.c
@@ -0,0 +1,76 @@
+#include "main.h"
+uint8_t usart_tx[10];
+uint8_t flash_data[W25Q16JV_SECTOR_SIZE] = {0};
+uint8_t flash_data_text[16] = {0};
+uint32_t current_addr = 0;
+
+void flash_dump(void)
+{
+    // printf("EEPROM WEL: %d\r\n", w25q16jv_read_reg1(W25Q16JV_REG1_WEL));
+    // printf("0x%08x     ", current_addr);
+    // for (uint8_t i = 0; i < 32; i++)
+    // {
+    //     // printf("**************BLOCK %d*****************\r\n", i);
+    //     for (uint8_t j = 0; j < 16; j++)
+    //     {
+    //         /* code */
+    //         // printf("**************SECTOR %d*****************\r\n", j);
+    //         w25q16jv_read_sector((i + 1) * j * W25Q16JV_SECTOR_SIZE - j * W25Q16JV_SECTOR_SIZE, flash_data);
+    //         for (uint32_t k = 0; k < W25Q16JV_SECTOR_SIZE; k++)
+    //         {
+    //             // printf("**************PAGE %d*****************\r\n", k);
+    //             sprintf(flash_data_text[k % 16], "%s", flash_data[i]);
+    //             printf("%02x ", flash_data[k]);
+    //             current_addr++;
+
+    //             if (current_addr % 16 == 0)
+    //                 printf("    %s\n0x%08x     ", flash_data_text, current_addr);
+    //             // printf("**************END PAGE %d**	***************\r\n", k);
+    //             // delay_1ms(50);
+    //         }
+    //         // printf("**************EEND SECTOR %d*****************\r\n", j);
+    //         // delay_1ms(50);
+    //     }
+    //     // printf("**************END BLOCK %d**************\r\n", i);
+    // }
+}
+
+void bk4819_test(void)
+{
+    // bk4819_write_reg(BK4819_REG_10, 0x8085);
+    // printf("BK4819 REG_10: 0x%x\r\n", bk4819_read_reg(BK4819_REG_10));
+    // printf("BK4819 REG_11: 0x%x\r\n", bk4819_read_reg(BK4819_REG_11));
+    // printf("BK4819 REG_12: 0x%x\r\n", bk4819_read_reg(BK4819_REG_12));
+    // printf("BK4819 REG_13: 0x%x\r\n", bk4819_read_reg(BK4819_REG_13));
+    // printf("BK4819 REG_14: 0x%x\r\n", bk4819_read_reg(BK4819_REG_14));
+    // printf("BK4819 REG_18: 0x%x\r\n", bk4819_read_reg(BK4819_REG_18));
+    // printf("BK4819 REG_19: 0x%x\r\n", bk4819_read_reg(BK4819_REG_19));
+
+    printf("Running...\r\n");
+    bk4819_set_freq(46255000);
+    printf("Current frequency: %d", (bk4819_read_reg(0x38) << 16 + bk4819_read_reg(0x39)) * 10);
+    printf("RSSI: %d\r\n", bk4819_read_reg(0x67) & 0x0F);
+    printf("Ex-noise: %d\r\n", bk4819_read_reg(0x65) & 0x0b111111);
+    printf("Glitch: %d\r\n", bk4819_read_reg(0x63) & 0x0b01111111);
+    printf("Squelch: %d\r\n", bk4819_read_reg(0x0C) & 0x01);
+}
+
+void bk1080_test(void)
+{
+
+    // bk1080_write_reg(BK1080_REG2, 0x8508);
+    // printf("Bk1080 REG0: 0x%x\r\n", bk1080_read_reg(BK1080_REG0));    // default 0x0006
+    printf("Bk1080 REG1: 0x%x\r\n", bk1080_read_reg(BK1080_REG1)); // default 0x1080
+    // printf("Bk1080 REG2: 0x%x\r\n", bk1080_read_reg(BK1080_REG2));    // default 0x0000
+    // printf("Bk1080 REG3: 0x%x\r\n", bk1080_read_reg(BK1080_REG3));    // default 0x0000
+    // printf("Bk1080 REG4: 0x%x\r\n", bk1080_read_reg(BK1080_REG4));    // default 0x0000
+    // printf("Bk1080 REG5: 0x%x\r\n", bk1080_read_reg(BK1080_REG5));    // default 0x0000
+    // printf("Bk1080 REG6: 0x%x\r\n", bk1080_read_reg(BK1080_REG6));    // default 0x0000
+    // printf("Bk1080 REG7: 0x%x\r\n", bk1080_read_reg(BK1080_REG7));    // default 0x0000
+    // printf("Bk1080 REG8: 0x%x\r\n", bk1080_read_reg(BK1080_REG8));    // default 0x0000
+    // printf("Bk1080 REG9: 0x%x\r\n", bk1080_read_reg(BK1080_REG9));    // default 0x0000
+    // printf("Bk1080 REG10: 0x%x\r\n", bk1080_read_reg(BK1080_REG10));  // default 0x0000
+    // printf("Bk1080 REG11: 0x%x\r\n", bk1080_read_reg(BK1080_REG11)); // default 0x0000
+
+    delay_1ms(1000);
+}
