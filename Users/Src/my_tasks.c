5dc67bbca847e0b76f27f177c2b727b61b8e2b36
Jamiexu <doxm@foxmail.com>
2024-06-24 17:23:38 +0800

    lvgl handler task

--- Users/Src/my_tasks.c
+++ Users/Src/my_tasks.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include "portmacro.h"
 
-void vTaskTestings(void *pvParameters)
-{
+// void vTaskTestings(void *pvParameters)
+// {
 
-    for (;;)
-    {
-        LCD_LIGHT_LOW;
-        vTaskDelay(1000);
-        LCD_LIGHT_HIGH;
-        vTaskDelay(1000);
-    }
-}
+//     for (;;)
+//     {
+//         LCD_LIGHT_LOW;
+//         vTaskDelay(1000);
+//         LCD_LIGHT_HIGH;
+//         vTaskDelay(1000);
+//     }
+// }
 
 void vTaskLvglRfer(void)
 {
@@ -21,11 +21,17 @@ void vTaskLvglRfer(void)
     }
 }
 
+void vTaskEvent(void)
+{
+    for (;;)
+    {
+        ui_event_cb();
+    }
+}
+
 void vtasks_init(void)
 {
-    // nvic_priority_group_set(NVIC_PRIGROUP_PRE0_SUB4);
-
-    xTaskCreate(vTaskTestings, "Test", 128, NULL, 0, NULL);
-    xTaskCreate(vTaskLvglRfer, "LCD handler", 512, NULL, 0, NULL);
+    xTaskCreate(vTaskEvent, "Event handler", 128, NULL, 0, NULL);
+    xTaskCreate(vTaskLvglRfer, "LCD refresh", 512, NULL, 0, NULL);
     timer_enable(TIMER1);
 }
