eb85cdd2e50b514edf2ed920cf0087a0e9597aa6
Jamie793 <doxm@foxmail.com>
2025-10-24 14:50:15 +0800

    key support long press and short press

--- Drivers/Key/key.c
+++ Drivers/Key/key.c
@@ -2,7 +2,12 @@
 
 // Written by Jamiexu
 
-key_map_t key_get(void)
+#define KEY_LONG_PRESS_THRESHOLD 500 // ms
+#define KEY_QUEUE_MAX 5
+QueueHandle_t keyQueue;
+static key_t key;
+
+static key_map_t key_get(void)
 {
     // Scan independent key
     gpio_bit_set(KEY_GPIO_PORT, KEY_GPIO_ROW0_PIN | KEY_GPIO_ROW1_PIN | KEY_GPIO_ROW2_PIN | KEY_GPIO_ROW3_PIN);
@@ -19,6 +24,7 @@ key_map_t key_get(void)
     {
         gpio_bit_set(KEY_GPIO_PORT, KEY_GPIO_ROW0_PIN | KEY_GPIO_ROW1_PIN | KEY_GPIO_ROW2_PIN | KEY_GPIO_ROW3_PIN);
         gpio_bit_reset(KEY_GPIO_PORT, 1 << 3 << i);
+        delay_1us(3);
         if (gpio_input_bit_get(KEY_GPIO_PORT, KEY_GPIO_COL0_PIN) == RESET)
             return i * 4 + 1;
         if (gpio_input_bit_get(KEY_GPIO_PORT, KEY_GPIO_COL1_PIN) == RESET)
@@ -29,4 +35,65 @@ key_map_t key_get(void)
             return i * 4 + 4;
     }
     return KEY_MAP_NONE;
+}
+
+void keyInit(void)
+{
+    keyQueue = xQueueCreate(KEY_QUEUE_MAX, sizeof(key_msg_t));
+    key.state = KEY_STATE_IDLE;
+    key.pressTime = 0;
+}
+
+void keyScan(void)
+{
+    uint32_t now = xTaskGetTickCount();
+    key_map_t k = key_get();
+    key_msg_t msg;
+
+    switch (key.state)
+    {
+    case KEY_STATE_IDLE:
+        if (k != KEY_MAP_NONE)
+        {
+            key.pressTime = xTaskGetTickCount();
+            msg.id = k;
+            msg.event = KEY_EVENT_PRESSED;
+            key.lastId = k;
+            xQueueSend(keyQueue, &msg, 0);
+            key.state = KEY_STATE_PRESSED;
+        }
+        break;
+    case KEY_STATE_PRESSED:
+        if (k != KEY_MAP_NONE)
+        {
+            if (now - key.pressTime >= KEY_LONG_PRESS_THRESHOLD)
+            {
+                msg.event = KEY_EVENT_LONG_PRESSED;
+                msg.id = key.lastId;
+                xQueueSend(keyQueue, &msg, 0);
+                key.state = KEY_STATE_LONG_PRESSED;
+            }
+        }
+        else
+        {
+            msg.id = key.lastId;
+            msg.event = KEY_EVENT_RELEASED;
+            xQueueSend(keyQueue, &msg, 0);
+            key.state = KEY_STATE_IDLE;
+        }
+        break;
+    case KEY_STATE_LONG_PRESSED:
+        if (k == KEY_MAP_NONE)
+        {
+            msg.id = key.lastId;
+            msg.event = KEY_EVENT_LONG_RELEASED;
+            xQueueSend(keyQueue, &msg, 0);
+            key.state = KEY_STATE_IDLE;
+        }
+        break;
+    }
+}
+
+uint32_t keyGetMsg(key_msg_t *msg){
+    return xQueueReceive(keyQueue, msg, 0);
 }
--- Drivers/Key/key.h
+++ Drivers/Key/key.h
@@ -9,14 +9,14 @@
 typedef enum
 {
     KEY_MAP_NONE = 0x00,
-    KEY_MAP_1,
-    KEY_MAP_2,
-    KEY_MAP_3,
-    KEY_MAP_4,
+    KEY_MAP_MENU,
+    KEY_MAP_UP,
+    KEY_MAP_DOWN,
+    KEY_MAP_RETURN,
     KEY_MAP_5,
     KEY_MAP_6,
     KEY_MAP_7,
-    KEY_MAP_8,
+    KEY_MAP_SWITCH,
     KEY_MAP_9,
     KEY_MAP_10,
     KEY_MAP_11,
@@ -24,13 +24,50 @@ typedef enum
     KEY_MAP_13,
     KEY_MAP_14,
     KEY_MAP_15,
-    KEY_MAP_16,
+    KEY_MAP_LOCK,
     KEY_MAP_TOP,
     KEY_MAP_PTT,
     KEY_MAP_L1,
     KEY_MAP_L2
 } key_map_t;
 
-key_map_t key_get(void);
+typedef enum
+{
+    KEY_STATE_IDLE = 0,
+    KEY_STATE_PRESSED,
+    KEY_STATE_LONG_PRESSED,
+    KEY_STATE_RELEASE,
+} key_state_t;
+
+typedef enum
+{
+    KEY_EVENT_NONE = 0,
+    KEY_EVENT_PRESSED,
+    KEY_EVENT_RELEASED,
+    KEY_EVENT_LONG_PRESSED,
+    KEY_EVENT_LONG_RELEASED
+} key_event_t;
+
+typedef struct 
+{
+    key_map_t id;
+    key_event_t event;
+} key_msg_t;
+
+
+typedef struct
+{
+    key_state_t state;   
+    key_map_t lastId;
+    uint32_t pressTime;    
+} key_t;
+
+
+void keyInit(void);
+
+void keyScan(void);
+
+uint32_t keyGetMsg(key_msg_t *msg);
+
 
 #endif
